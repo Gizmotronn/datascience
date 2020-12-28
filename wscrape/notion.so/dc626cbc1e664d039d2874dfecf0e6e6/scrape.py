3460102354c30ed3b3aa9cfe3952ee32c77fac58
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-12-28 16:38:25 +0000

    Static website scraping + content for course #1

---
+++ wscrape/notion.so/dc626cbc1e664d039d2874dfecf0e6e6/scrape.py
@@ -0,0 +1,6 @@
+import requests
+
+url = "https://www.indeed.com/jobs?q=python&l=new+york"
+response = requests.get(url)
+
+# https://www.notion.so/skinetics/Scraping-w-beautiful-soup-dc626cbc1e664d039d2874dfecf0e6e6#11ca7f3317df4538b2ac810530dc8da8
