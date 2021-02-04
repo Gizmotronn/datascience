a932d70575cc6c76b666111f68f4458e7991f270
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-04 03:55:15 +0000

    DSP-9 ; error

---
+++ flask/social/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from .extensions import bootstrap, db, moment
+from config import config
+
+def create_app(config_name):
+    app = Flase(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].imit_app(app)
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    moment.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
--- flask/social/app.py
+++
@@ -1,7 +0,0 @@
-# Import required modules/libraries
-from flask import flask
-
-# Setup application
-app = Flask(__name__)
-
-@app.route('')
