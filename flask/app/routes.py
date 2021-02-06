92c18426bdf7072a80e97dcea83077b56053066b
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-06 14:58:07 +0000

    DSP-10: Finish Web Forms

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -18,11 +18,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST']) # view function for login
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
-            form.username.date, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index')) # Dynamically fetches the url defined in "index" view, or function, in routes.py
+    return render_template('login.html', title='Sign In', form=form) # returns the file in app/templates/login.html which extends from base.html
