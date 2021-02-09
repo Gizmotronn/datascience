8d75d75ae29001845d04dfc87f0be17c19508b79
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-09 04:26:20 +0000

    DSP-10: Logging users in

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
+from flask_login import current_user, login_user
 from app.forms import LoginForm
+from app.models import User
+from flask_login import logout_user
 
 @app.route('/')
 @app.route('/index')
@@ -20,9 +23,14 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST']) # view function for login
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index')) # Dynamically fetches the url defined in "index" view, or function, in routes.py
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or note user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('logi'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form) # returns the file in app/templates/login.html which extends from base.html
